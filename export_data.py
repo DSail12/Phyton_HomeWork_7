8d8e6751e90401918e6564b18aed65e631e42e22
dsail <dsail@yandex.ru>
2022-11-15 15:49:17 +0300

    commit9

--- export_data.py
+++ export_data.py
@@ -6,16 +6,16 @@ def export_data():
             if ',' in line:
                 temp = line.strip().split(',')
                 data.append(temp)
-                elif ';' in line:
-                    temp = line.strp().split(';')
-                    data.append(temp)
-                elif ':' in line:
+            elif ';' in line:
+                temp = line.strp().split(';')
+                data.append(temp)
+            elif ':' in line:
                     temp = line.strp().split(':')
                     data.append(temp)
-                elif line != '':
+            elif line != '':
                     if line != '\n':
                         t.append(line.strip())
-                    else:
-                        data.append(t)
-                        t= []
-return data
+            else:
+                data.append(t)
+                t= []
+    return data
